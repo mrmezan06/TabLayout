7951c249ba293e52c4e31d4ab779467fdcb571e6
MEZAN <mrmezan06@gmail.com>
2019-04-26 12:35:05 +0600

    Initial commit

---
+++ app/src/main/java/com/mezan/tablayout/MainActivity.java
@@ -0,0 +1,46 @@
+package com.mezan.tablayout;
+
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    TabLayout tabLayout;
+    ViewPager viewPager;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        tabLayout=(TabLayout)findViewById(R.id.tabLayout);
+        viewPager=(ViewPager)findViewById(R.id.viewPager);
+
+        tabLayout.addTab(tabLayout.newTab().setText("Home"));
+        tabLayout.addTab(tabLayout.newTab().setText("Sport"));
+        tabLayout.addTab(tabLayout.newTab().setText("Movie"));
+        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+
+        final MyAdapter adapter = new MyAdapter(this,getSupportFragmentManager(), tabLayout.getTabCount());
+        viewPager.setAdapter(adapter);
+
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/mezan/tablayout/MyAdapter.java
@@ -0,0 +1,41 @@
+package com.mezan.tablayout;
+
+import android.content.Context;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.app.FragmentManager;
+
+public class MyAdapter extends FragmentPagerAdapter {
+
+    private Context myContext;
+    int totalTabs;
+
+    public MyAdapter(Context context, FragmentManager fm, int totalTabs) {
+        super(fm);
+        myContext = context;
+        this.totalTabs = totalTabs;
+    }
+
+    // this is for fragment tabs
+    @Override
+    public Fragment getItem(int position) {
+        switch (position) {
+            case 0:
+                HomeFragment homeFragment = new HomeFragment();
+                return homeFragment;
+            case 1:
+                SportsFragment sportFragment = new SportsFragment();
+                return sportFragment;
+            case 2:
+                MovieFragment movieFragment = new MovieFragment();
+                return movieFragment;
+            default:
+                return null;
+        }
+    }
+    // this counts total number of tabs
+    @Override
+    public int getCount() {
+        return totalTabs;
+    }
+}
